fbc1857a27d7bb67b4a9a4d723344750bbd7f90d
gabrielhicks <gabrielhicks@mail.com>
2020-09-12 12:14:43 -0400

    Create game flow

--- app/models/cli.rb
+++ app/models/cli.rb
@@ -3,7 +3,15 @@ require 'tty-prompt'
 require 'colorize'
 require 'tty-box'
 class CLI
-    def self.main_screen
+
+    def welcome_screen
+        box = TTY::Box.frame(width: 30, height: 8, align: :center, border: :thick, padding: 2) do "Welcome to A Trail, a tale of misadventure!" 
+        end
+        puts box
+        sleep (2)
+    end
+
+    def start_game
         system("clear")
         prompt = TTY::Prompt.new
         selection = prompt.select("Welcome, to the 'A Trail' game! Please select an option:") do |menu|
@@ -11,9 +19,11 @@ class CLI
             menu.choice name: "Quit".colorize(:red), value: 2
         end
         if selection == 2
-            box = TTY::Box.frame(width: 30, height: 8, align: :center, border: :thick, padding: 2) do "Thanks for playing, it has been great to get to know you!" end
-                sleep(3)
-                exit!
+            box = TTY::Box.frame(width: 30, height: 8, align: :center, border: :thick, padding: 2) do "Thanks for playing, it has been great to get to know you!" 
+            end
+            puts box
+            sleep(3)
+            exit
         else selection == 1
             return true
         end
@@ -21,20 +31,26 @@ class CLI
 
     def start
         system("clear")
-        prompt = TTY::Prompt.new
-        box = TTY::Box.frame(width: 30, height: 8, align: :center, border: :thick, padding: 2) do "Welcome to A Trail, a tale of misadventure!" end
-        sleep (2)
-        user = User.login
+        self.welcome_screen
+        @user = self.login
+        self.start_game
         user.create_skills
-        user.start_game
-        choose_char = prompt.select("Please choose a character to play:") do |menu|
-            menu.choice name: "Gabriel Hicks", value: 1
+        User.choose_char
+    end
+
+    def login
+        prompt = TTY::Prompt.new
+            selection = prompt.select("Hello! Please sign up, or sign in:") do |menu|
+                menu.choice name: "New User".colorize(:green), value: 1
+                menu.choice name: "Existing User".colorize(:green), value: 2
+                menu.choice name: "Exit".colorize(:red), value: 3
             end
-        if choose_char == 1
-            system("clear")
-            UserStory.find_or_create_by(user_id: user.id, story_id: Story.first.id)
-            box = TTY::Box.frame(width: 30, height: 8, align: :center, border: :thick, padding: 2) do "Enjoy your new life as Gabriel Hicks" end
-            Bio.first.menu
-        end
+                if selection == 1
+                    @user = User.create_user
+                elsif selection == 2
+                    @user = User.existing_user
+                elsif selection == 3
+                    exit
+                end
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -11,13 +11,36 @@ class User < ActiveRecord::Base
     has_many :user_stories
     has_one :story, through: :user_stories
 
-    def self.login
-        system("clear")
+    def self.create_user
+        prompt = TTY::Prompt.new
+        name = prompt.ask("Please create a username")
+        if User.find_by(username: name) == nil
+            prompt = TTY::Prompt.new
+            password = prompt.mask("Please create a password")
+            full_name = prompt.ask("Please enter your first and last name")
+            return User.find_or_create_by(username: name, password: password, first_name: full_name.split(" ").first, last_name: full_name.split(" ").last)
+        else
+            box = TTY::Box.warn("It appears this username has already been taken")
+            print box
+            sleep(3)
+            User.create_user
+        end
+    end
+
+    def self.existing_user
         prompt = TTY::Prompt.new
         name = prompt.ask("Please enter your username")
         password = prompt.mask("Please enter your password")
-        User.find_or_create_by(username: name, password: password)
+        if User.find_by(username: name, password: password) == nil
+            box = TTY::Box.warn("Invalid Username/Password combonation, please try again.")
+            print box
+            sleep(3)
+            User.existing_user
+        else
+        return User.find_by(username: name, password: password)
+        end
     end
+
 
     def create_skills
         if self.user_skills.empty? 
@@ -25,19 +48,15 @@ class User < ActiveRecord::Base
         end
     end
 
-    def start_game
-        system("clear")
-        prompt = TTY::Prompt.new
-        selection = prompt.select("Welcome, to the 'A Trail' game! Please select an option:") do |menu|
-            menu.choice name: "Start Game".colorize(:green), value: 1
-            menu.choice name: "Quit".colorize(:red), value: 2
+    def choose_char
+        choose_char = prompt.select("Please choose a character to play:") do |menu|
+        menu.choice name: "Gabriel Hicks", value: 1
         end
-        if selection == 2
-            box = TTY::Box.frame(width: 30, height: 8, align: :center, border: :thick, padding: 2) do "Thanks for playing, it has been great to get to know you!" end
-                sleep(3)
-                exit!
-        else selection == 1
-            return true
+        if choose_char == 1
+            system("clear")
+            UserStory.find_or_create_by(user_id: user.id, story_id: Story.first.id)
+            box = TTY::Box.frame(width: 30, height: 8, align: :center, border: :thick, padding: 2) do "Enjoy your new life as Gabriel Hicks" end
+            Bio.first.menu
         end
     end
 end
